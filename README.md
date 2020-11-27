df59f5ea3ac732fb049f9441732aa718657b5835
David Groomes <dwgroomes@gmail.com>
2020-11-26 21:19:58 -0600

    Share version info via contextBridge

--- README.md
+++ README.md
@@ -18,9 +18,18 @@ Requires: Node.js
 1. Install dependencies with `npm install`
 1. Run the app with `npm start`
 
+## Materials Referenced
+
+Below are useful materials I referenced while developing this repository. Many of these are worth revisiting and
+reviewing.
+
+* [Electron official site: Tutorial on "Context Isolation"](https://www.electronjs.org/docs/tutorial/context-isolation)
+* [StackOverflow answer about "preload" scripts](https://stackoverflow.com/a/59814127)
+* [Electron official site: API for "BrowserWindow"](https://www.electronjs.org/docs/api/browser-window)
+
 ## Wish List
 
 General clean ups, TODOs and things I wish to implement for this project:
 
-* How does the "preload" lifecycle work exactly? How does ipc work? How do Web Workers work? I'd like to try injecting
+* DONE How does the "preload" lifecycle work exactly? How does ipc work? How do Web Workers work? I'd like to try injecting
   the version data using one of these methods instead of what I'm doing in the `detect-versions.js` script.  
--- detect-versions.js
+++ detect-versions.js
@@ -1,36 +1,16 @@
 /*
- * Detects versions of Chromium, Node, Electron and writes them into the DOM. NOTE: for static information like these
- * version numbers I would prefer to write the version numbers directly into the HTML before shipping the HTML document
- * to Chromium. In other words, I would prefer to server-side render (SSR) the content instead of rendering the content
- * after-the-fact via DOM manipulation in JavaScript code that uses the Document APIs. SSR seems like a better order in
- * my opinion, in this case. Interestingly, the Electron programming model seems to bias towards the "Single-Page
- * Application" (SPA) paradigm. See this commentary: https://stackoverflow.com/a/24618996. This is perfectly fine and my
- * development experience with Electron so far has been really good. The docs are great. So I'll continue to bear in
- * mind the SPA bias because it is idiomatic but I am curious to model an Electron app with SSR.
+ * Detects versions of Chromium, Node, and Electron and shares those values via the "contextBridge" API to the renderer
+ * process/Chromium. Read about the "contextBridge" at https://www.electronjs.org/docs/tutorial/context-isolation and
+ * https://www.electronjs.org/docs/api/context-bridge
  *
- * This will run as a "preload" script. For more info about how "preload" scripts work see the documentation for
- * BrowserWindow and specifically scroll down to the "webPreferences" option: https://www.electronjs.org/docs/api/browser-window
+ * This JavaScript code will be run as a "preload" script. For more information about how "preload" scripts work see the
+ * documentation for BrowserWindow and specifically scroll down to the "webPreferences" option: https://www.electronjs.org/docs/api/browser-window
  */
 
-console.log("Hello from 'detect-versions.js'");
+const {contextBridge} = require('electron')
 
-window.addEventListener('DOMContentLoaded', () => {
-    console.log("Hello from 'detect-versions.js' after 'DOMContentLoaded'");
-    {
-        let element = document.getElementById(`chrome-version`);
-        let version = process.versions['chrome'];
-        element.innerText = `Version: ${version}`;
-    }
-
-    {
-        let element = document.getElementById(`node-version`);
-        let version = process.versions['node'];
-        element.innerText = `Version: ${version}`;
-    }
-
-    {
-        let element = document.getElementById(`electron-version`);
-        let version = process.versions['electron'];
-        element.innerText = `Version: ${version}`;
-    }
+contextBridge.exposeInMainWorld("myData", {
+    chromeVersion: process.versions['chrome'],
+    nodeVersion: process.versions['node'],
+    electronVersion: process.versions['electron']
 });
