6ea2cf3fb99d02c40f6d80d643305e050fd79e46
David Groomes <dwgroomes@gmail.com>
2023-12-29 11:13:18 -0600

    [`core-only`] Upgrade to Electron 28 and drop the `src/` directory

--- core-only/app.js
+++ core-only/app.js
@@ -1,6 +1,6 @@
 /*
 This file is the primary source of JavaScript for the web page (i.e. the renderer process and Chromium). By contrast,
-"index.js" is the entrypoint for the Electron application's "Main" process which executes in the Node.js runtime
+"main.js" is the entrypoint for the Electron application's "main" process which executes in the Node.js runtime
 environment.
 */
 
