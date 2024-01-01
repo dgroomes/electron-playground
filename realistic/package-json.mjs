46a59ee1392e352ef32cda2c8ac7fa4eecc4beca
David Groomes <dwgroomes@gmail.com>
2023-12-31 22:14:40 -0600

    [`realistic/`] Upgrade dependencies (except Forge)

--- realistic/package-json.mjs
+++ realistic/package-json.mjs
@@ -16,17 +16,17 @@ maintainability, especially for people who are new to the project (or me returni
 import fs from "fs";
 
 const versions = {
-    typescript: "^5.2", // TypeScript releases: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-2.html
-    electron: "^27.0.4", // Electron releases: https://releases.electronjs.org/releases/stable
-    electronForge: "^6.4.2", // Electron Forge releases: https://github.com/electron/forge/releases
-    tsLoader: "^9.5.0", // ts-loader releases: https://github.com/TypeStrong/ts-loader/releases
-    webpackDevServer: "^4.15.1", // webpack-dev-server releases: https://github.com/webpack/webpack-dev-server/releases/tag/v4.15.1
-    htmlWebpackPlugin: "^5.5.3", // html-webpack-plugin releases: https://github.com/jantimon/html-webpack-plugin/blob/main/CHANGELOG.md
-    react: "^18.2.0", // React releases: https://legacy.reactjs.org/versions
-    reactTypes: "^18.2.37", // @types/react releases: https://www.npmjs.com/package/@types/react
-    reactDomTypes: "^18.2.15", // @types/react-dom releases: https://www.npmjs.com/package/@types/react-dom
-    cssLoader: "^6.8.1", // css-loader releases: https://github.com/webpack-contrib/css-loader/releases
-    styleLoader: "^3.3.3", // style-loader releases: https://github.com/webpack-contrib/style-loader/releases
+    typescript: "~5.3", // TypeScript releases: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-3.html
+    electron: "~28.1.0", // Electron releases: https://releases.electronjs.org/releases/stable
+    electronForge: "~6.4.2", // Electron Forge releases: https://github.com/electron/forge/releases
+    tsLoader: "~9.5.1", // ts-loader releases: https://github.com/TypeStrong/ts-loader/releases
+    webpackDevServer: "~4.15.1", // webpack-dev-server releases: https://github.com/webpack/webpack-dev-server/releases/tag/v4.15.1
+    htmlWebpackPlugin: "~5.6.0", // html-webpack-plugin releases: https://github.com/jantimon/html-webpack-plugin/blob/main/CHANGELOG.md
+    react: "~18.2.0", // React releases: https://legacy.reactjs.org/versions
+    reactTypes: "~18.2.46", // @types/react releases: https://www.npmjs.com/package/@types/react?activeTab=versions
+    reactDomTypes: "~18.2.18", // @types/react-dom releases: https://www.npmjs.com/package/@types/react-dom?activeTab=versions
+    cssLoader: "~6.8.1", // css-loader releases: https://github.com/webpack-contrib/css-loader/releases
+    styleLoader: "~3.3.3", // style-loader releases: https://github.com/webpack-contrib/style-loader/releases
 };
 
 /**
